0224da33d64d7bb2c0cac01f6c23080ffda73b68
Untss <76573735+Untss@users.noreply.github.com>
2021-03-12 21:01:27 +0100

    Add files via upload

---
+++ HangMan Discord Bot/Bot.cs
@@ -0,0 +1,62 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Interactivity.Extensions;
+using HangMan_Discord_Bot.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan_Discord_Bot
+{
+    class Bot
+    {
+
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set;  }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var config = new DiscordConfiguration
+            {
+                Token = "ODE5OTkxMjg2Njg5MDM4MzY3.YEuqZQ._aLZz2g7ZbvZo8tJm9bypB5lpWE",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.UseInteractivity(new InteractivityConfiguration(){
+                PollBehaviour = PollBehaviour.KeepEmojis,
+                Timeout = TimeSpan.FromSeconds(30)
+            });
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] {"."},
+                EnableDms = false,
+                EnableMentionPrefix = true,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<StartCommand>();
+            Commands.RegisterCommands<GuessCommand>();
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
