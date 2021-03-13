bfccb387ff8a16c985fd50e3125093fd651aeea0
Untss <76573735+Untss@users.noreply.github.com>
2021-03-13 16:25:01 +0100

    Add files via upload

---
+++ HangMan Discord Bot/Commands/StartCommand.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan_Discord_Bot.Commands
+{
+    public class StartCommand : BaseCommandModule
+    {
+
+        [Command("start")]
+        public async Task start(CommandContext ctx)
+        {
+            Game.Game.setupGame();
+            DiscordEmbed embed = new DiscordEmbedBuilder();
+
+            var builder = new DiscordEmbedBuilder()
+            {
+                Title = "New Game!",
+                Color = DiscordColor.CornflowerBlue,    
+                Description = "use .guess <char> to guess a char!"
+            };
+
+            string txt = "";
+
+            for(int i = 0; i < Game.Game.word.Length; i++)
+            {
+                txt = txt + "_ ";
+            }
+
+            builder.AddField("Tries Left", $"{7 - Game.Game.wrongCounter}", false);
+            builder.AddField("Guessed Chars", $"```{txt}```", false);
+
+            await ctx.Channel.SendMessageAsync(builder);
+        }
+
+
+    }
+}
---
+++ HangMan Discord Bot/Game/Game.cs
@@ -0,0 +1,45 @@
+﻿using HangMan_Discord_Bot.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HangMan_Discord_Bot.Game
+{
+    class Game
+    {
+        public static string word;
+        public static List<String> words = new List<String>(File.ReadAllLines("Words.txt"));
+        public static List<String> guessedChars = new List<String>();
+        public static int wrongCounter = 0;
+        public static Boolean isPlaying = false;
+
+        public static void setupGame()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(words.Count);
+            word = words[index];
+            isPlaying = true;
+        }
+
+        public static void GuessWord(string s)
+        {
+            if (word.Contains(s))
+            {
+                guessedChars.Add(s);
+            }
+            else
+            {
+                wrongCounter++;
+            }
+        }
+
+        public static void resetGame()
+        {
+            isPlaying = false;
+            word = null;
+            wrongCounter = 0;
+            guessedChars.Clear();
+        }
+    }
+}
